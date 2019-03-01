2434ba968473fe2ace5823869c644b0218c22df9
Tony <tonywzhang@gmail.com>
2019-03-01 12:18:11 -0800

    Part 5 Start

--- lib/05_aggregates.rb
+++ lib/05_aggregates.rb
@@ -22,24 +22,41 @@ end
 def continents
   # List all the continents - just once each.
   execute(<<-SQL)
+    SELECT DISTINCT
+      continent
+    FROM
+      countries
   SQL
 end
 
 def africa_gdp
   # Give the total GDP of Africa.
   execute(<<-SQL)
+    SELECT
+      SUM(gdp)
+    FROM
+      countries
+    WHERE
+      continent = 'Africa'
   SQL
 end
 
 def area_count
   # How many countries have an area of more than 1,000,000?
   execute(<<-SQL)
+    SELECT
+      COUNT(name)
+    FROM
+      countries
+    WHERE
+      area > 1000000
   SQL
 end
 
 def group_population
   # What is the total population of ('France','Germany','Spain')?
   execute(<<-SQL)
+    
   SQL
 end
 
