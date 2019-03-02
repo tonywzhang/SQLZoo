8589d322799bb92a914394a39b1771a9a7ee85c2
Tony <tonywzhang@gmail.com>
2019-03-01 17:26:51 -0800

    Part 6 COmpletion

--- lib/03a_nobel_bonus.rb
+++ lib/03a_nobel_bonus.rb
@@ -14,6 +14,11 @@ require_relative './sqlzoo.rb'
 def physics_no_chemistry
   # In which years was the Physics prize awarded, but no Chemistry prize?
   execute(<<-SQL)
-    
+    SELECT
+      yr
+    FROM
+      nobels
+    GROUP BY
+      yr 
   SQL
 end
